a80749ee628eaf3cb1c075c49d7461f569e24c82
phloreenm <florinmaracine@yahoo.com>
2023-01-20 01:05:53 +0000

    Added error msg in script; tried using template literals, but error; added defer for script loading

--- script.js
+++ script.js
@@ -1,15 +1,25 @@
     let list_container = "";
     list_container = document.getElementById('results-list');
 
+    // var start_s = "01-06-2022";
+    // var end_s = "03-06-2022";
+
     $("button").click(function () {
         $.ajax({
+            // url: `https://odegdcpnma.execute-api.eu-west-2.amazonaws.com/development/prices?dno=10&voltage=HV&start=${start_s}&end=${end_s}}`,
             url: 'https://odegdcpnma.execute-api.eu-west-2.amazonaws.com/development/prices?dno=10&voltage=HV&start=01-06-2022&end=03-06-2022',
+            // headers: {'Content-Type': 'application/json'},
+            // type: "GET",
+            // dataType: "json",
             success: function (data) {
                 // print response data in the console
                 console.log(JSON.stringify(data.status));
                 console.log(JSON.stringify(data));
                 // clear the content of the results list container
-                document.getElementById("results-list").innerHTML = " ";
+                let res_lst = document.getElementById("results-list");
+                res_lst.innerHTML = " ";
+                res_lst.insertAdjacentHTML('beforeend', `<p>What are DNOs? Distribution Network Operators (DNOs) are licensed companies that own and operate the network of towers, transformers, cables and meters that carry electricity from the national transmission system and distribute it throughout Britain.</p>`);
+                res_lst.insertAdjacentHTML('beforeend', `<p>"Overall" price in p/kWh, the "unixTimestamp", and "Timestamp" with the specific time and date.</p>`);
                 // append the response results objects in the HTML page
                 var obj = data;
                 if (obj.status == "success") {
@@ -24,7 +34,12 @@
                         //   document.body.appendChild(paragrah);
                     });
                 }
+            },
+            error: function () {
+                console.log("Error! Couldn't get data.");
+                // console.trace();
             }
 
         })
     });
+    
