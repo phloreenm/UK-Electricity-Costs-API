f9ee09cfde1941d660b86cc354206389745b119e
phloreenm <florinmaracine@yahoo.com>
2023-01-20 00:17:46 +0000

    Added comments to script code

--- script.js
+++ script.js
@@ -5,8 +5,12 @@
         $.ajax({
             url: 'https://odegdcpnma.execute-api.eu-west-2.amazonaws.com/development/prices?dno=10&voltage=HV&start=01-06-2022&end=03-06-2022',
             success: function (data) {
+                // print response data in the console
+                console.log(JSON.stringify(data.status));
                 console.log(JSON.stringify(data));
+                // clear the content of the results list container
                 document.getElementById("results-list").innerHTML = " ";
+                // append the response results objects in the HTML page
                 var obj = data;
                 if (obj.status == "success") {
                     var count = 0;
